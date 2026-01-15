a3a9a3d69d1d81f733345efab05800e52794c79d
zolika <zolika>
2026-01-15 10:16:05 +0100

    nemtommi

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Todo</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<input type="text" id="title" placeholder="Új todo">
-<button id="add">Hozzáadás</button>
+<div class="add-box">
+    <input type="text" id="title" placeholder="Új todo">
+    <button id="add">Hozzáadás</button>
 
 <script src="script.js"></script>
 </body>
